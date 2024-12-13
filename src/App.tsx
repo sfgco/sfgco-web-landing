3e09097c1855b8f6752bf8868ae076802db24f8b
Ahmed Khaled <ahmedkhaled4d@gmail.com>
2024-12-13 14:08:30 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <img src="/logo.png" className="logo" alt="SFGCO logo" />
+      <h1> مستقبل الاستدامه الخضراء للاستدامه و الاستثمار</h1>
+      <p className="read-the-docs">info@sfgco.sa</p>
+    </>
+  );
+}
+
+export default App;
